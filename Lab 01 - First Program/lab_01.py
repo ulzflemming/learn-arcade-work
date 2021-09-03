810b745d8f3761b3ab104db4f5a8c7287c114c86
ulzflemming <garmilasforbus@gmail.com>
2021-09-03 17:24:02 -0500

    drawing_samples

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,1 @@
-Hi
- https://github.com/ulzflemming/learn-arcade-work/tree/master/Lab%2001%20-%20First%20Program
+print ("hello world")
---
+++ Testing/test.py
@@ -0,0 +1,8 @@
+import arcade
+print("""You can print
+on multiple 
+lines using
+triple
+quotes.""")
+
+print("You you you \nNext \nnext next")
